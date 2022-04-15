f695b0551ddbf651e24f9aff20d38004397281bd
michaelaboah <maboah@PlexusParc.com>
2022-04-15 16:27:13 -0400

    Working: Added Open File and Save File to the menu

--- src/electron/menu.ts
+++ src/electron/menu.ts
@@ -1,52 +1,152 @@
-import { Menu, MenuItem, } from "electron";
+import { app, BrowserWindow, dialog, } from "electron";
+import fs  from "fs";
+import { mainWindow } from ".";
 
-const template = [
-  {
-    label: 'Test',
+const isMac = process.platform === 'darwin'
+
+export const template = [
+  // { role: 'appMenu' }
+  ...(isMac ? [{
+    label: app.name,
     submenu: [
-      {
-        label: 'submenu1'
-      }
-    ],
-    
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideOthers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  }] : []),
+  // { role: 'fileMenu' }
+  {
+    label: 'File',
+    submenu: [
+      isMac ? { role: 'close' } : { role: 'quit' },
+      {label: 'New'},
+      {label: 'New Window'},
+      {type: 'separator'},
+      {label: 'Open', accelarator: "CmdOrCtrl+O", click() {openFile()}},
+      {label: 'Open Recent'},
+      {type: 'separator'},
+      {label: 'Save', accelerator: "CommandOrControl+S"},
+      {label: 'Save As', accelarator: "CommandOrControl+Shift+S", click() {saveAsFile()}},
+    ]
+  },
+  // { role: 'editMenu' }
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(isMac ? [
+        { role: 'pasteAndMatchStyle' },
+        { role: 'delete' },
+        { role: 'selectAll' },
+        { type: 'separator' },
+        {
+          label: 'Speech',
+          submenu: [
+            { role: 'startSpeaking' },
+            { role: 'stopSpeaking' }
+          ]
+        }
+      ] : [
+        { role: 'delete' },
+        { type: 'separator' },
+        { role: 'selectAll' }
+      ])
+    ]
+  },
+  // { role: 'viewMenu' }
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forceReload' },
+      { role: 'toggleDevTools' },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  },
+  // { role: 'windowMenu' }
+  {
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' },
+      ...(isMac ? [
+        { type: 'separator' },
+        { role: 'front' },
+        { type: 'separator' },
+        { role: 'window' }
+      ] : [
+        { role: 'close' }
+      ])
+    ]
   },
   {
-    label: 'Test',
+    role: 'help',
     submenu: [
       {
-        label: 'submenu1'
+        label: 'Learn More',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://electronjs.org')
+        }
       }
-    ],
-    
+    ]
   }
 ]
 
-export const menuExtended: MenuItem[] = [
-    new MenuItem({
-        label: "Cloud",
-        submenu: [
-            {
-              label: 'Sync'
-            },
-            {
-            label: 'Sync'
-            },
-            {
-            label: 'Sync'
-            },
-            {
-            label: 'Sync'
-            },
-          ],
-    }),
-    new MenuItem({
-        label: 'Test',
-        submenu: [
-          {
-            label: 'submenu1'
-          }
-        ],
+
+
+// *************************** Menu Functions **************************
+
+const openFile = async():Promise<any> =>{
+    const { canceled, filePaths } = await dialog.showOpenDialog(mainWindow as BrowserWindow,{
+        properties: ['openFile'],
+        filters: [
+            {name: 'JSON', extensions: ['json']},
+            {name: '*', extensions: ['fyr']}
+        ]});
+    if (canceled) {
+        return;
+    }
+    fs.readFile(filePaths[0],(err, data) =>{
+        if(err){
+            return console.log(err)
+        }
+        else data
     })
-]
+}
 
-
+const saveAsFile = async() =>{
+    const { canceled, filePath } = await dialog.showSaveDialog(mainWindow as BrowserWindow,{
+        properties: ['createDirectory'],
+        filters: [
+            {name: 'Json', extensions: ['json']},
+            {name: 'Freyur', extensions: ['fyr']}
+        ]});
+    if (canceled) {
+        return;
+    }
+    else{
+        fs.writeFile(filePath as string, "InsertData Here Later", function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        }); 
+    }
+}
